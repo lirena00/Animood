94b7642f5673d5560c36ea496d5e8b5a5c987e18
lirena <sakshamkushwaha12776@gmail.com>
2024-04-20 14:05:58 +0530

    formatted and optimized code

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,25 +11,6 @@ module.exports = {
         "primary":"#121520",
         "secondary":"#212b42",
         "action":"#23A9D5",
-        "accent":"white"
-      },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      animation: {
-        "meteor-effect": "meteor 5s linear infinite",
-      },
-      keyframes: {
-        meteor: {
-          "0%": { transform: "rotate(215deg) translateX(0)", opacity: "1" },
-          "70%": { opacity: "1" },
-          "100%": {
-            transform: "rotate(215deg) translateX(-500px)",
-            opacity: "0",
-          },
-        },
       },
     },
   },
